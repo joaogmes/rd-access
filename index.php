1acc40235d9da1b05ff1ecd5ffbd90d23045b169
Joao Gomes <joao-pedrogomes@hotmail.com>
2023-07-17 22:54:34 -0300

    Setup slim framework

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
-echo "hi";
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write("API is working on port 8080!");
+    return $response;
+});
+
+$app->run();
--- setup.sh
+++ setup.sh
@@ -2,4 +2,8 @@ sudo apt update &&
 sudo apt install php8.1 -y &&
 sudo apt install php8.1-mysql -y &&
 sudo apt install php8.1-curl -y &&
-sudo apt install mysql-server -y
+sudo apt install mysql-server -y &&
+sudo apt install npm -y &&
+sudo apt install composer -y &&
+composer require slim/slim &&
+composer require slim/psr7
