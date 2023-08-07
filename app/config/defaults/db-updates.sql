0e1f1058b8f2fe4a1cc22be2263f8cf14514dc35
Joao Gomes <joao-pedrogomes@hotmail.com>
2023-08-07 00:57:22 -0300

    Fix Authorization table

--- app/config/defaults/db-updates.sql
+++ app/config/defaults/db-updates.sql
@@ -1,2 +1,9 @@
-ALTER TABLE `Access`
-	CHANGE COLUMN `creationDate` `creationDate` DATETIME NULL DEFAULT NOW() AFTER `code`;
+ALTER TABLE
+	`Access` CHANGE COLUMN `creationDate` `creationDate` DATETIME NULL DEFAULT NOW()
+AFTER
+	`code`;
+
+ALTER TABLE
+	`Authorization` CHANGE COLUMN `corePrefix` `codePrefix` VARCHAR(250) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci'
+AFTER
+	`codeCore`;
