bc3df35af0cfb4bcab76041693066af6a2ff8f2e
Joao Gomes <joao-pedrogomes@hotmail.com>
2023-08-08 22:27:02 -0300

    Added database creation on setup script

--- database.sh
+++ database.sh
@@ -1,4 +1,20 @@
-sudo mysql && CREATE USER 
-~              
-~              
-~              
+sudo mysql <<EOF
+DROP USER 'rd'@'localhost';
+FLUSH PRIVILEGES;
+CREATE USER 'rd'@'localhost' IDENTIFIED BY 'access';
+GRANT ALL PRIVILEGES ON *.* TO 'rd'@'localhost';
+FLUSH PRIVILEGES;
+EXIT;
+EOF
+
+# MySQL credentials
+mysql_user="rd"
+mysql_password="access"
+
+# Define the path to the SQL file
+creationFile="app/config/defaults/db-creation.sql"
+updateFile="app/config/defaults/db-updates.sql"
+
+# Execute the SQL file using MySQL
+mysql -u $mysql_user -p$mysql_password <$creationFile
+mysql -u $mysql_user -p$mysql_password <$updateFile
--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 sudo apt update &&
 sudo apt install net-tools -y &&
 sudo apt install php8.1 -y &&
-sudo apt install php8.1-mysql -y &&
+# sudo apt install php8.1-mysql -y &&
+ sudo apt install mariadb-server-10.0 -y &&
 sudo apt install php8.1-curl -y &&
 sudo apt install mysql-server -y &&
 sudo apt install npm -y &&
 sudo apt install composer -y &&
 composer require slim/slim &&
 composer require slim/psr7
+./database.sh
