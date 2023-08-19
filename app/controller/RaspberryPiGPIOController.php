c1d671120689bd229abef4da1b8d5995c1a3569d
Joao Gomes <joao-pedrogomes@hotmail.com>
2023-08-19 14:21:52 -0300

    Changed gpio error time to microseconds

--- app/controller/RaspberryPiGPIOController.php
+++ app/controller/RaspberryPiGPIOController.php
@@ -83,18 +83,18 @@ class RaspberryPiGPIOController
         switch ($error) {
             case "invalid":
                 for ($i = 0; $i < 3; $i++) {
-                    sleep(0.5);
+                    usleep(500000);
                     $this->write("red", "off");
-                    sleep(0.5);
+                    usleep(500000);
                     $this->write("red", "on");
                 }
                 break;
             case "repeated":
                 for ($i = 0; $i < 3; $i++) {
-                    sleep(0.5);
+                    usleep(500000);
                     $this->write("red", "off");
                     $this->write("green", "on");
-                    sleep(0.5);
+                    usleep(500000);
                     $this->write("red", "on");
                     $this->write("green", "off");
                 }
