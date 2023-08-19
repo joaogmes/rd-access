661af8c2902d8471930ae40ce68036c1ef04336a
Joao Gomes <joao-pedrogomes@hotmail.com>
2023-08-19 13:58:21 -0300

    Create quick teerminal setup for testing

--- app/model/ConfigModel.php
+++ app/model/ConfigModel.php
@@ -13,18 +13,29 @@ class ConfigModel
         $this->dao = new ConfigDao($this->table);
     }
 
-    public function verifyTerminalSetup(){
+    public function verifyTerminalSetup()
+    {
         $verifyQuery = "SELECT * FROM Config";
         $configRegisters = $this->dao->list($verifyQuery);
-        if($configRegisters['total'] != 1){
+        if ($configRegisters['total'] != 1) {
             $this->resetConfig();
+            if (is_numeric($this->setNewConfig())) {
+                return true;
+            }
             return false;
         }
         return $configRegisters['results'][0];
     }
 
-    public function resetConfig(){
+    public function resetConfig()
+    {
         $resetQuery = $this->dao->execute("TRUNCATE Config");
     }
 
+    private function setNewConfig()
+    {
+        $macAddress = trim(shell_exec("/sbin/ifconfig eth0 | grep -oE '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'"));
+        $configInsert = "INSERT INTO `RdAccessTerminal`.`Config` (`macAddress`) VALUES ('{$macAddress}')";
+        return $this->dao->insert($configInsert);
+    }
 }
