3521d9df0a74333555c80913b9255fc7365b0285
Joao Gomes <joao-pedrogomes@hotmail.com>
2023-08-19 15:47:34 -0300

    Update on init.sh adding logs to it

--- init.sh
+++ init.sh
@@ -1,2 +1,12 @@
-cd ~/rd-access/ &&
-php init.php
+#!/bin/bash
+
+# This script runs the specified commands.
+
+# Log the script start
+echo "Script started" >> /home/joao/rd-access/init.log
+
+# Change directory to rd-access and run init.php using PHP
+cd ~/rd-access/ && php init.php
+
+# Log completion (optional)
+echo "Script completed" >> /home/joao/rd-access/init.log
