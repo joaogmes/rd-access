3ea9c3f1b08582835e4d5d38cbfbdb804a4a0445
Joao Gomes <joao-pedrogomes@hotmail.com>
2023-08-19 15:55:37 -0300

    Test init.sh with nohup on php execution

--- init.sh
+++ init.sh
@@ -5,8 +5,8 @@
 # Log the script start
 echo "Script started" >> /home/joao/rd-access/init.log
 
-# Change directory to rd-access and run init.php using PHP
-cd ~/rd-access/ && php init.php
+# Change directory to rd-access and run init.php using PHP in the background
+cd ~/rd-access/ && nohup php init.php &
 
 # Log completion (optional)
 echo "Script completed" >> /home/joao/rd-access/init.log
