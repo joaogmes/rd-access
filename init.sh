972667602faeb5a406e84026c2138d67644dab92
Joao Gomes <joao-pedrogomes@hotmail.com>
2023-08-19 17:00:42 -0300

    New init script test

--- init.sh
+++ init.sh
@@ -3,10 +3,10 @@
 # This script runs the specified commands.
 
 # Log the script start
-echo "Script started" >> /home/joao/rd-access/init.log
-
-# Change directory to rd-access and run init.php using PHP in the background
+# echo "Script started" >> /home/joao/rd-access/init.log
+sudo systemctl start mysql
 cd ~/rd-access/ && nohup php init.php &
+# Change directory to rd-access and run init.php using PHP in the background
 
 # Log completion (optional)
-echo "Script completed" >> /home/joao/rd-access/init.log
+# echo "Script completed" >> /home/joao/rd-access/init.log
